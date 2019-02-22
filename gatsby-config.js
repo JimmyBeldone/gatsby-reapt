de7191ac453301288c20a0a4422635d16d405bd7
Jimmy Beldone <dev.jimmy.beldone@gmail.com>
2019-02-22 18:20:38 +0100

    feat: typo

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,7 @@ module.exports = {
     },
     plugins: [
         `gatsby-plugin-react-helmet`,
+        // `gatsby-plugin-typography`,
         {
             resolve: `gatsby-source-filesystem`,
             options: {
@@ -37,9 +38,9 @@ module.exports = {
                 display: `minimal-ui`,
                 icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
             }
-        }
+        },
         // this (optional) plugin enables Progressive Web App + Offline functionality
         // To learn more, visit: https://gatsby.app/offline
-        // 'gatsby-plugin-offline',
+        "gatsby-plugin-offline"
     ]
 };
