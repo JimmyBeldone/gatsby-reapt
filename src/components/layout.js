de02112dceafe02cbf1d722eea61f54f87300014
Jimmy Beldone <dev.jimmy.beldone@gmail.com>
2019-02-22 18:21:22 +0100

    fix: style import order

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,6 +4,7 @@ import { StaticQuery, graphql } from "gatsby";
 
 import Header from "./header";
 import "./layout.css";
+import "./global.styl";
 
 const Layout = ({ children }) => (
     <StaticQuery
