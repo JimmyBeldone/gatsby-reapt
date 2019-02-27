d0b87bd1e08513f4404477f44c78f98bb524a51c
Jimmy Beldone <dev.jimmy.beldone@gmail.com>
2019-02-27 14:55:34 +0100

    feat(setup): script to remove original .git/  and update package.json

---
+++ setup/setupMessage.js
@@ -0,0 +1,6 @@
+console.log(" =========================== ");
+console.log(" =    Gatsby Reapt Setup   = ");
+console.log(" =========================== \n");
+console.log(" Installing dependencies ...\n");
+console.log(" Go get a coffee...\n");
+console.log(" =========================== \n");
