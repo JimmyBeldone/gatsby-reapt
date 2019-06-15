387cbeb5330421014f10cb354bf37f57fc3aeb3c
Jimmy Beldone <dev.jimmy.beldone@gmail.com>
2019-06-15 04:44:20 +0200

    feat: add gatsby-plugin-offline

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,16 @@
 import "typeface-montserrat";
 import "typeface-lato";
 
+import "./src/styles/vendors/reset.styl";
 import "./src/styles/layout.styl";
 import "./src/styles/global.styl";
-import ReduxWrapper from "./src/providers/ReduxWrapper";
 
-export const wrapRootElement = ReduxWrapper;
+export const onServiceWorkerUpdateReady = () => window.location.reload(true);
+
+export const onClientEntry = () => {
+    // Without this function body the import will not be picked up.
+};
+
+// import ReduxWrapper from "./src/providers/ReduxWrapper";
+
+// export const wrapRootElement = ReduxWrapper;
