655b0925e0c45e03dcb71e835d0a7806a6be487c
Jimmy Beldone <dev.jimmy.beldone@gmail.com>
2019-06-16 02:13:49 +0200

    chore: add fallback value if no env file defined

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,8 +13,8 @@ module.exports = {
         title: `demo.meta.title`,
         description: `demo.meta.description`,
         author: `Jimmy Beldone`,
-        siteUrl: process.env.SITE_URL,
-        apiUrl: process.env.API_URL,
+        siteUrl: process.env.SITE_URL || "http://localhost:8000",
+        apiUrl: process.env.API_URL || "http://localhost.api",
         social: {
             twitter: `kylemathews`
         }
