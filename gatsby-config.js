fc3a6c6a612ca60819af8acd023069834fee88e7
Jimmy Beldone <dev.jimmy.beldone@gmail.com>
2019-06-16 17:21:08 +0200

    chore: config eslint & prettier

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
-const styleResources = require("./src/styles/styleConfig");
+const styleResources = require('./src/styles/styleConfig');
 
-let activeEnv = process.env.MODE || process.env.NODE_ENV || "development";
+let activeEnv = process.env.MODE || process.env.NODE_ENV || 'development';
 console.log(`Using environment config: '${activeEnv}'`);
 
 // eslint-disable-next-line import/no-extraneous-dependencies
-require("dotenv").config({
-    path: `.env.${activeEnv}`
+require('dotenv').config({
+    path: `.env.${activeEnv}`,
 });
 
 module.exports = {
@@ -13,49 +13,50 @@ module.exports = {
         title: `demo.meta.title`,
         description: `demo.meta.description`,
         author: `Jimmy Beldone`,
-        siteUrl: process.env.SITE_URL || "http://localhost:8000",
-        apiUrl: process.env.API_URL || "http://localhost.api",
+        siteUrl: process.env.SITE_URL || 'http://localhost:8000',
+        apiUrl: process.env.API_URL || 'http://localhost.api',
         social: {
-            twitter: `kylemathews`
-        }
+            twitter: `dev.jimmy`,
+        },
     },
     plugins: [
         `gatsby-plugin-react-helmet`,
         {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `images`,
-                path: `${__dirname}/src/images`
-            }
-        },
-        {
-            resolve: "gatsby-plugin-stylus-resources",
-            options: {
-                resources: styleResources
-            }
-        },
-        {
-            resolve: "gatsby-plugin-eslint",
+            resolve: 'gatsby-plugin-eslint',
             options: {
                 test: /\.js$|\.jsx$/,
                 exclude: /(node_modules|.cache|public)/,
-                stages: ["develop"],
+                stages: ['develop'],
                 options: {
                     emitWarning: true,
-                    failOnError: false
-                }
-            }
+                    failOnError: false,
+                },
+            },
         },
         {
-            resolve: "gatsby-plugin-typography",
+            resolve: 'gatsby-plugin-typography',
             options: {
-                pathToConfigModule: "config/typography.js"
-            }
+                pathToConfigModule: 'config/typography.js',
+            },
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                path: `${__dirname}/src/images`,
+                name: `images`,
+            },
+        },
+        {
+            resolve: 'gatsby-plugin-stylus-resources',
+            options: {
+                resources: styleResources,
+            },
         },
         `gatsby-transformer-sharp`,
         `gatsby-plugin-sharp`,
-        `gatsby-plugin-advanced-sitemap`,
         `gatsby-plugin-feed`,
+        `gatsby-plugin-netlify`,
+        `gatsby-plugin-advanced-sitemap`,
         {
             resolve: `gatsby-plugin-manifest`,
             options: {
@@ -70,65 +71,64 @@ module.exports = {
                     {
                         src: `/favicons/icon-48x48.png`,
                         sizes: `48x48`,
-                        type: `image/png`
+                        type: `image/png`,
                     },
                     {
                         src: `/favicons/icon-72x72.png`,
                         sizes: `72x72`,
-                        type: `image/png`
+                        type: `image/png`,
                     },
                     {
                         src: `/favicons/icon-96x96.png`,
                         sizes: `96x96`,
-                        type: `image/png`
+                        type: `image/png`,
                     },
                     {
                         src: `/favicons/icon-144x144.png`,
                         sizes: `144x144`,
-                        type: `image/png`
+                        type: `image/png`,
                     },
                     {
                         src: `/favicons/icon-192x192.png`,
                         sizes: `192x192`,
-                        type: `image/png`
+                        type: `image/png`,
                     },
                     {
                         src: `/favicons/icon-256x256.png`,
                         sizes: `256x256`,
-                        type: `image/png`
+                        type: `image/png`,
                     },
                     {
                         src: `/favicons/icon-384x384.png`,
                         sizes: `384x384`,
-                        type: `image/png`
+                        type: `image/png`,
                     },
                     {
                         src: `/favicons/icon-512x512.png`,
                         sizes: `512x512`,
-                        type: `image/png`
-                    }
-                ]
-            }
+                        type: `image/png`,
+                    },
+                ],
+            },
         },
         {
-            resolve: "gatsby-plugin-robots-txt",
+            resolve: 'gatsby-plugin-robots-txt',
             options: {
                 resolveEnv: () => activeEnv,
                 env: {
                     development: {
-                        policy: [{ userAgent: "*", disallow: ["/"] }]
+                        policy: [{ userAgent: '*', disallow: ['/'] }],
                     },
                     staging: {
-                        policy: [{ userAgent: "*", allow: "/" }]
+                        policy: [{ userAgent: '*', allow: '/' }],
                     },
                     production: {
-                        policy: [{ userAgent: "*", allow: "/" }]
-                    }
-                }
-            }
+                        policy: [{ userAgent: '*', allow: '/' }],
+                    },
+                },
+            },
         },
-        "gatsby-plugin-offline",
-        "gatsby-plugin-webpack-bundle-analyser-v2"
-        // `gatsby-plugin-netlify`
-    ]
+        'gatsby-plugin-offline',
+        'gatsby-plugin-webpack-bundle-analyser-v2',
+    ],
 };
