ebd0de2dc4f592497b53e44480be3aac2a15d4d2
Jimmy Beldone <dev.jimmy.beldone@gmail.com>
2019-09-18 15:00:09 +0200

    fix: remove intl polyfill

--- src/providers/I18nProvider.js
+++ src/providers/I18nProvider.js
@@ -1,14 +1,6 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { IntlProvider } from 'react-intl';
-import '@formatjs/intl-relativetimeformat/polyfill';
-/*
-    Locale 'en' is imported by default,
-    To load additional locale, you can include them on demand like below.
-    If you want to polyfill all locales :
-    import '@formatjs/intl-relativetimeformat/polyfill-locales'
-*/
-import '@formatjs/intl-relativetimeformat/dist/locale-data/fr';
 
 import en from '../lang/en/';
 import fr from '../lang/fr/';
