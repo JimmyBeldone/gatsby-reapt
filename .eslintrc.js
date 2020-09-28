831b01c4a4eded62fc6b25dca6de6002af9710cc
Jimmy Beldone <dev.jimmy.beldone@gmail.com>
2020-09-28 22:11:43 +0200

    chore: update eslint conf

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,8 +8,12 @@ module.exports = {
         'universe/web',
         'plugin:jsx-a11y/recommended',
     ],
-    plugins: ['jsx-a11y'],
+    plugins: ['jsx-a11y', 'sort-keys-fix', 'sort-destructure-keys'],
     rules: {
+        'react/no-unused-prop-types': 1,
         'react/prop-types': 1,
+        'react/sort-prop-types': 1,
+        'sort-destructure-keys/sort-destructure-keys': 1,
+        'sort-keys-fix/sort-keys-fix': 'warn',
     },
 };
