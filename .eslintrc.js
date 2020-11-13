2430e2bff6652f1a88dfb8f890655eaa7ec6f748
Jimmy Beldone <dev.jimmy.beldone@gmail.com>
2020-11-13 21:58:16 +0100

    fix: update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,43 @@
 module.exports = {
-    // globals: {
-    //     __PATH_PREFIX__: true,
-    // },
+    env: {
+        browser: true,
+        node: true,
+    },
+
     extends: [
-        'universe/node',
-        // 'universe/shared/react',
-        'universe/web',
-        'plugin:jsx-a11y/recommended',
+        'plugin:react/recommended',
+        'airbnb',
+        'airbnb/hooks',
+        'prettier',
+        'prettier/react',
     ],
-    plugins: ['jsx-a11y', 'sort-keys-fix', 'sort-destructure-keys'],
+    globals: {
+        __PATH_PREFIX__: true,
+    },
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    // parser: 'babel-eslint',
+    plugins: ['prettier', 'sort-keys-fix', 'sort-destructure-keys'],
+    root: true,
     rules: {
+        'implicit-arrow-linebreak': 0,
+        indent: [0, 4],
+        'jsx-quotes': ['error', 'prefer-single'],
+        'no-console': 0,
+        'object-curly-newline': 0,
+        'operator-linebreak': 0,
+        'prettier/prettier': 'error',
+        'react-hooks/exhaustive-deps': 'warn',
+        'react-hooks/rules-of-hooks': 'error',
+        'react/forbid-prop-types': 0,
+        'react/jsx-indent': 0,
+        'react/jsx-indent-props': 0,
+        'react/jsx-props-no-spreading': 0,
         'react/no-unused-prop-types': 1,
         'react/prop-types': 1,
         'react/sort-prop-types': 1,
